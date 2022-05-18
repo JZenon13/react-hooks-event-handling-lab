1c3d2ea8c04ce56e5941bea3012d645eda083611
jesse zenon <jesse.zenon@gmail.com>
2022-05-18 13:57:15 -0500

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+function handleClick(e) {
+  if (e._reactName === "onBlur") {
+    console.log("Hey! Eyes on me!");
+  } else {
+    console.log("Good!");
+  }
+}
+function EyesOnMe() {
+  return (
+    <button onBlur={handleClick} onFocus={handleClick}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import React from "react";
+function handleChange(e) {
+  console.log("Entering password...");
+}
+
+function Keypad() {
+  return <input type="password" onChange={handleChange}></input>;
+}
+
+export default Keypad;
